0c64690eae13769ba180c7456712d349163daa0c
Alex Sandro <geniofuturo@gmail.com>
2017-01-06 17:28:53 -0200

    Including pip dependencies.

--- setup.py
+++ setup.py
@@ -1,4 +1,19 @@
 from distutils.core import setup
+import os
+
+basedir = os.path.dirname(os.path.basename(__file__))
+
+try:
+    from pip.req import parse_requirements
+    from pip.download import PipSession
+
+    install_reqs = parse_requirements(os.path.join(basedir, 'requirements.txt'),
+                                      session=PipSession())
+
+    install_reqs = [str(ir.req) for ir in install_reqs]
+
+except ImportError:
+    install_reqs = []
 
 setup(
     name='xadmin-mptt-plugin',
@@ -8,5 +23,6 @@ setup(
     license='MIT',
     author='alex',
     author_email='',
-    description='Xadmin plugin editing templates on the tree'
+    description='Xadmin plugin editing templates on the tree',
+    install_reqs=install_reqs
 )
