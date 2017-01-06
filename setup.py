dbc9d9f369db9d2ca0351437636938821ddd52d1
Alex Sandro <geniofuturo@gmail.com>
2017-01-06 17:00:22 -0200

    Implementing setup.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='xadmin-mptt-plugin',
+    version='1.0',
+    packages=['xplugin'],
+    url='https://github.com/alexsilva/Xadmin-FAQ',
+    license='MIT',
+    author='alex',
+    author_email='',
+    description='Xadmin plugin editing templates on the tree'
+)
