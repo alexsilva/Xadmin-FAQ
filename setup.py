4edbb53134643c0e8614ab8321a2af74f2678227
alexsilva <geniofuturo@gmail.com>
2018-08-30 13:30:07 -0300

    django-mptt to django <= 1.9.9

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ basedir = os.path.dirname(os.path.basename(__file__))
 
 setup(
     name='xadmin-mptt-plugin',
-    version='1.0',
+    version='1.1',
     packages=['xplugin_mptt'],
     url='https://github.com/alexsilva/xadmin-mppt-plugin',
     license='MIT',
     author='alex',
     author_email='',
     description='Xadmin plugin editing templates on the tree',
-    install_requires=["django-mptt"]
+    install_requires=["django-mptt==0.9.0"]
 )
